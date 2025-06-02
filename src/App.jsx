7aa411dda7012da5f1253c6921efb6708761ac3f
Soham-Kale <sohamkale3014@gmail.com>
2025-06-02 13:38:32 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl font-bold text-red-500'>Helle Everyone, Let's build Netflix GPT</div>
+    </>
+  )
+}
+
+export default App;
